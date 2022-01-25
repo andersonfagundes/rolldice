98db14a435e34361adfb65d09da6ba364dd72a8a
Anderson Fagundes <anderson.fagundes4@gmail.com>
2022-01-25 09:32:48 -0300

    Removido insertData e comentarios de DiceController

--- src/main/java/br/edu/infnet/controller/DiceController.java
+++ src/main/java/br/edu/infnet/controller/DiceController.java
@@ -20,33 +20,13 @@ public class DiceController {
 	@Autowired
 	DiceService diceService;
 
-	/*
-	@GetMapping("/start")
-	public ResponseEntity<String> rollDices(@RequestParam(name = "type") Integer face,
-								 @RequestParam(name = "amount") Integer amount) {
-
-		Integer result = diceService.rollDices(face, amount);
-
-		return new ResponseEntity<>("{value:"+result+"}", HttpStatus.OK);
-	}*/
-
-//	@GetMapping(value = "/iniciativa", produces = MediaType.APPLICATION_JSON_VALUE)
-//	public ResponseEntity<Map<String, Object>> rollDices(@RequestParam(name = "type") Integer face,
-//														 @RequestParam(name = "amount") Integer amount) {
-//
-//		Integer result = diceService.rollDices(face, amount);
-//
-//		Map<String, Object> payload = new HashMap<>();
-//		payload.put("value", result);
-//
-//		return ResponseEntity.ok(payload);
-//	}
-
 	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Map<String, Object>> getResultRollDices(@RequestParam(name = "type") Integer face,
 																	@RequestParam(name = "amount") Integer amount) {
 		Integer result = diceService.rollDices(face, amount);
-
+		
+		//Thread.sleep(7000);
+		
 		Map<String, Object> payload = new HashMap<>();
 		payload.put("value", result);
 		
